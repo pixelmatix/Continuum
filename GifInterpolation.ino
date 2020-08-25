23f56ee5ca34001ba90fd39521180c4db57bcebb
Louis Beaudoin <louis@embedded-creations.com>
2020-08-25 10:04:25 +0100

    Customize generic parameters sketch for this application

--- GifInterpolation.ino
+++ GifInterpolation.ino
@@ -72,7 +72,7 @@
  *    Use matrix.setMaxCalculationCpuPercentage() or matrix.setCalcRefreshRateDivider()
  */
 
-#include <SmartLEDShieldV4.h>  // comment out this line for if you're not using SmartLED Shield V4 hardware (this line needs to be before #include <SmartMatrix3.h>)
+#include <MatrixHardware_T4Adapter.h>   // Teensy 4 Adapter attached to SmartLED Shield for Teensy 3 V4
 #include <SmartMatrix3.h>
 #include "Layer_BackgroundInterpolation.h"
 
@@ -80,7 +80,7 @@
 #include <GifDecoder.h>
 #include "FilenameFunctions.h"
 
-#define DISPLAY_TIME_SECONDS 10
+#define DISPLAY_TIME_SECONDS (5*60)
 #define NUMBER_FULL_CYCLES   1
 
 #define USE_SMARTMATRIX         1
@@ -93,12 +93,18 @@ const int defaultBrightness = 255;
 #if (USE_SMARTMATRIX == 1)
 /* SmartMatrix configuration and memory allocation */
 #define COLOR_DEPTH 24                  // known working: 24, 48 - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24
-const uint8_t kMatrixWidth = 32;        // known working: 32, 64, 96, 128
-const uint8_t kMatrixHeight = 32;       // known working: 16, 32, 48, 64
+#if 1
+const uint8_t kMatrixWidth = 96;        // known working: 32, 64, 96, 128
+const uint8_t kMatrixHeight = 96;       // known working: 16, 32, 48, 64
+const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN; // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels, or use SMARTMATRIX_HUB75_64ROW_MOD32SCAN for common 64x64 panels
+#else
+const uint8_t kMatrixWidth = 64;        // known working: 32, 64, 96, 128
+const uint8_t kMatrixHeight = 64;       // known working: 16, 32, 48, 64
+const uint8_t kPanelType = SMARTMATRIX_HUB75_64ROW_MOD32SCAN; // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels, or use SMARTMATRIX_HUB75_64ROW_MOD32SCAN for common 64x64 panels
+#endif
 const uint8_t kRefreshDepth = 36;       // known working: 24, 36, 48
 const uint8_t kDmaBufferRows = 2;       // known working: 2-4
-const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN; // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels, or use SMARTMATRIX_HUB75_64ROW_MOD32SCAN for common 64x64 panels
-const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);    // see http://docs.pixelmatix.com/SmartMatrix for options
+const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_BOTTOM_TO_TOP_STACKING);    // see http://docs.pixelmatix.com/SmartMatrix for options
 const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
 const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
 
@@ -230,11 +236,7 @@ void setup() {
 #endif
 
     matrix.setBrightness(defaultBrightness);
-    matrix.setRefreshRate(240);
-
-
-    // for large panels, may want to set the refresh rate lower to leave more CPU time to decoding GIFs (needed if GIFs are playing back slowly)
-    //matrix.setRefreshRate(90);
+    matrix.setRefreshRate(250);
 
 #if !defined(ESP32)
     matrix.begin();
